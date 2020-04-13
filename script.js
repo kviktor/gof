c66505591074335bd55938f6480d069661c2cba9
Viktor Kálmán <vkalman@consumeraffairs.com>
2020-04-14 00:35:56 +0200

    add gun pattern

--- script.js
+++ script.js
@@ -18,7 +18,13 @@ var patterns = {
     [2, 1], [3, 1], [4, 1], [6, 2], [6, 3], [6, 4], [4, 6], [3, 6], [2, 6], [1, 4], [1, 3], [1, 2],
     [2, -1], [3, -1], [4, -1], [6, -2], [6, -3], [6, -4], [4, -6], [3, -6], [2, -6], [1, -4], [1, -3], [1, -2],
     [-2, 1], [-3, 1], [-4, 1], [-6, 2], [-6, 3], [-6, 4], [-4, 6], [-3, 6], [-2, 6], [-1, 4], [-1, 3], [-1, 2],
-  ]
+  ],
+  'gun': [
+    [0, 0], [-1, 0], [-1, 1], [-1, -1], [-2, -2], [-2, 2], [-3, 0], [-4, 3], [-4, -3], [-5, 3], [-5, -3], [-6, -2], [-6, 2], [-7, 1], [-7, 0], [-7, -1],
+    [3, -1], [3, -2], [3, -3], [4, -1], [4, -2], [4, -3], [5, -4], [5, 0], [7, 0], [7, 1], [7, -4], [7, -5],
+    [17, -2], [17, -3], [18, -2], [18, -3],
+    [-16, 0], [-17, 0], [-16, -1], [-17, -1],
+  ],
 }
 
 
@@ -32,6 +38,7 @@ class Control {
     var fasterButton = document.getElementById('faster');
     var startButton = document.getElementById('start');
     var pauseButton = document.getElementById('pause');
+
     this.speed = document.getElementById('speed');
     this.performance = document.getElementById('performance');
     this.interval = null;
@@ -341,6 +348,6 @@ class Manager {
 
     this.cells = filtered;
 
-    this.latestPerformance = performance.now() -t0;
+    this.latestPerformance = performance.now() - t0;
   }
 }
